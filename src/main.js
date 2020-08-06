bf5d232dc3a450f60abac8f0a66e542f5513c0e3
rph <rphsoftware@gmail.com>
2020-08-06 23:05:49 +0200

    Unlocking webaudio

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
 // This script shouldn't do anything without explicit user interaction (Triggering playback)
 require("regenerator-runtime/runtime");
 const browserCapabilities = require('./browserCapabilities');
+const unlock = require('./webAudioUnlock');
+let hasInitialized = false;
+let capabilities = null;
+let audioContext = null;
+let scriptNode = null;
+let gainNode = null;
+
+async function startPlaying(url) {
+    if (!hasInitialized) {
+        capabilities = await browserCapabilities();
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        unlock(audioContext);
+    }
+}
+
 window["initializePlayer"] = async function(url) {
     // Check browser capabilities
     console.time("capability");
     console.log(await browserCapabilities());
     console.timeEnd("capability");
 
-    let z = new Uint8Array(2**28);
-    z.fill(255);
 
-    let zz = new Uint8Array(2**28);
+}
 
-    console.time("Copy");
-    zz.set(z, 0);
-    console.timeEnd("Copy");
+window.player = {
+    play: startPlaying
 }
---
+++ src/webAudioUnlock.js
@@ -0,0 +1,34 @@
+module.exports = function(ac) {
+    return new Promise(function(resolve) {
+        let unlockWrapper = document.createElement("div");
+        unlockWrapper.style = `background: #888a; z-index: 88888; position: fixed; top: 0; bottom: 0; left: 0; right: 0; display: flex; align-items: center; justify-content: center;`
+        let unlockPrompt = document.createElement("div");
+        unlockPrompt.style = `display: flex; align-items: center; justify-content: center; flex-direction: column`;
+        unlockPrompt.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" version="1.0"  width="200" height="200" viewBox="0 0 75 75">
+<path d="M39.389,13.769 L22.235,28.606 L6,28.606 L6,47.699 L21.989,47.699 L39.389,62.75 L39.389,13.769z"
+style="stroke:#fff;stroke-width:5;stroke-linejoin:round;fill:#fff;"
+/>
+<path d="M48,27.6a19.5,19.5 0 0 1 0,21.4M55.1,20.5a30,30 0 0 1 0,35.6M61.6,14a38.8,38.8 0 0 1 0,48.6" style="fill:none;stroke:#fff;stroke-width:5;stroke-linecap:round"/>
+</svg><h1 style="font-family: sans-serif; color: white; margin: 0;">Tap or click anywhere to enable audio.</h1>`;
+
+        unlockWrapper.appendChild(unlockPrompt);
+
+        document.body.appendChild(unlockWrapper);
+
+        unlockWrapper.addEventListener("touchend", function() {
+            ac.resume();
+            if (ac.state === "running") {
+                resolve();
+                unlockWrapper.remove();
+            }
+        });
+
+        unlockWrapper.addEventListener("click", function() {
+            ac.resume();
+            if (ac.state === "running") {
+                resolve();
+                unlockWrapper.remove();
+            }
+        })
+    });
+}
